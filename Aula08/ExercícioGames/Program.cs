bccae0a44159d555155e4113363d93b268d66b69
apaixav <lucasrib01@gmail.com>
2023-08-31 15:57:27 -0300

    Objetos

--- Aula08/ExercícioGames/Program.cs
+++ Aula08/ExercícioGames/Program.cs
@@ -7,6 +7,8 @@ namespace ExercícioGames
         static void Main(string[] args)
         {
             Games g1 = new Games("Shadow of the Colossus",5 ,99.5,12,9.7);
+            Games g2 = new Games("Elden Ring", 4, 299.90, 17, 9.1);
+
 
             g1.Visualizar();
             g1.SetPreço(150.00);
@@ -19,6 +21,16 @@ namespace ExercícioGames
             g1.Desconto(1000);
 
             g1.Visualizar();
+
+            g2.Visualizar();                      
+
+            g2.Aumento(20.00);
+
+            g2.Visualizar();
+
+            g2.Desconto(70);
+
+            g2.Visualizar();
         }
     }
 }
--- Aula08/ExercícioObjeto/Program.cs
+++ Aula08/ExercícioObjeto/Program.cs
@@ -7,6 +7,7 @@ namespace ExercícioObjeto
         static void Main(string[] args)
         {
             Cliente c1 = new Cliente("Lucas", "267.593.722-64", 27, true, 1);
+            Cliente c2 = new Cliente("Clara", "726.953.271-46", 22, true, 2);
 
             c1.Visualizar();
             c1.SetIdade(36);
@@ -19,6 +20,16 @@ namespace ExercícioObjeto
             c1.SetGenero(3);
 
             c1.Visualizar();
+
+            c2.Visualizar();
+            
+            c2.Dois(true);
+
+            c2.Visualizar();
+
+            c2.SetGenero(2);
+
+            c2.Visualizar();
         }
     }
 }
